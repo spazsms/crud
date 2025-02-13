745e7eda3e1770ed89f666226ebfb1b4de23d3ea
huni lex <hunilecks@gmail.com>
2025-02-13 14:37:49 +0800

    migrations/model

---
+++ app/Models/CarouselItems.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class CarouselItems extends Model
+{
+    use HasFactory;
+
+    protected $table = 'carousel_items';
+    protected $primaryKey = 'carousel_id';
+    protected $fillable = [
+        'carousel_name',
+        'image_path',
+        'description',
+        'user_id',
+    ];
+}
